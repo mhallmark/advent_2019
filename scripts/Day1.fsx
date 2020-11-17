c661236c91f778dc621d32e9e88fc29cab652967
Matthew Hallmark <matthall1155@gmail.com>
2020-11-16 17:42:54 -0800

    dw

--- scripts/Day1.fsx
+++ scripts/Day1.fsx
@@ -2,9 +2,28 @@
 
 let massToFuelRatio (mass: int) = mass / 3 - 2
 
-let computeFuel (filePath: string) =
-    File.ReadLines(filePath)
+let moduleFuel =
+    File.ReadLines("data/day1-input.txt")
     |> Seq.map (int)
     |> Seq.sumBy (massToFuelRatio)
 
-printfn "%i" <| computeFuel "data/day1-input.txt"
+printfn "Module fuel: %i" moduleFuel
+
+let mutable fuels = [ massToFuelRatio moduleFuel ]
+
+while fuels.Head > 0 do
+    let fuelMassFuel =
+        match massToFuelRatio fuels.Head with
+        | sub when sub < 0 -> 0
+        | n -> n
+
+    fuels <- fuelMassFuel :: fuels
+
+Seq.iter (printfn "More fuel mass: %i") fuels
+
+let fuelFuel = fuels |> Seq.sum
+
+printfn "Fuel fuel: %i" fuelFuel
+
+let totalFuel = moduleFuel + fuelFuel
+printfn "Total fuel: %i" totalFuel
