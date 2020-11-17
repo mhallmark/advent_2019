366818bd946c473d84806764c4d40edb41d9a43d
Matthew Hallmark <matthall1155@gmail.com>
2020-11-16 16:18:34 -0800

    oops

---
+++ scripts/Day1.fsx
@@ -0,0 +1,10 @@
+﻿open System.IO
+
+let massToFuelRatio (mass: int) = mass / 3 - 2
+
+let computeFuel (filePath: string) =
+    File.ReadLines(filePath)
+    |> Seq.map (int)
+    |> Seq.sumBy (massToFuelRatio)
+
+printfn "%i" <| computeFuel "data/day1-input.txt"
